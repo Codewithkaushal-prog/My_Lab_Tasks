8d0533f1b9f9f5a95411e1b9c57683479915a171
Codewithkaushal-prog <meb25050@tezu.ac.in>
2025-10-23 16:07:07 +0530

    added 2 files
    added helloworld.c , intro.c

---
+++ LAB_01_Task/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    printf("Kaushal Kumar\n");
+    printf("Roll No. : MEB25050\n");
+    printf("Department : Mechanical Engineering\n");
+    printf("Tezpur University\n");
+    return 0;
+}
